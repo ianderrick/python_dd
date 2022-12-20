d4f4baa9bafe564c46f0803b0123dadf36c7c555
Ian Derrick <15677606+ianderrick@users.noreply.github.com>
2022-12-20 08:04:31 -0800

    Update pydd.py

--- pydd.py
+++ pydd.py
@@ -1,53 +1,53 @@
 import os
 from tkinter import *
 
-# create the root window
+# Create the root window
 root = Tk()
 
-# create a label to display the instructions
+# Create a label to display the instructions
 label = Label(root, text="Select the input and output devices for the dd command:")
 label.pack()
 
-# create a variable to store the input and output devices
+# Create a variable to store the input and output devices
 input_device = ""
 output_device = ""
 
-# create a function to get the input device
+# Create a function to get the input device
 def get_input_device():
   global input_device
   input_device = input_device_entry.get()
 
-# create a function to get the output device
+# Create a function to get the output device
 def get_output_device():
   global output_device
   output_device = output_device_entry.get()
 
-# create a function to run the dd command
+# Create a function to run the dd command
 def run_dd_command():
-  # build the dd command using the input and output devices
+# Build the dd command using the input and output devices
   dd_command = "dd if=" + input_device + " of=" + output_device + " bs=1M"
-  # run the dd command
+# Run the dd command
   os.system(dd_command)
 
-# create a text entry for the input device
+# Create a text entry for the input device
 input_device_entry = Entry(root)
 input_device_entry.pack()
 
-# create a button to get the input device
+# Create a button to get the input device
 input_device_button = Button(root, text="Input Device", command=get_input_device)
 input_device_button.pack()
 
-# create a text entry for the output device
+# Create a text entry for the output device
 output_device_entry = Entry(root)
 output_device_entry.pack()
 
-# create a button to get the output device
+# Create a button to get the output device
 output_device_button = Button(root, text="Output Device", command=get_output_device)
 output_device_button.pack()
 
-# create a button to run the dd command
+# Create a button to run the dd command
 dd_command_button = Button(root, text="Run dd Command", command=run_dd_command)
 dd_command_button.pack()
 
-# start the GUI event loop
+# Start the GUI event loop
 root.mainloop()
